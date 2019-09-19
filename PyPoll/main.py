42393ae3fdcb28badbbddc72483a575dfc8c789f
Farshad E <farshade@Farshads-MacBook-Pro.local>
2019-09-18 23:29:45 -0500

    Updte as of 18 Sept 2019

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -1,29 +1,58 @@
-#main.pay in PyPoll
-candidates = {'name': [] ,'votes': []}
+# main.pay in PyPoll
+# Farshad Esnaashari
+# Data Anlytics and Visualization M-W
 
-answer= "y"
-votes = 0
-while answer =="y":
-    candidate = input("Enter the Candidate Name:  ")
+# import packages
+import os
+import csv
 
-    print(f" from the keyboard{ candidate}")
+# Set the path for the csv file
+election_data_csv = os.path.join("Resources", "election_data.csv")
 
-    list1 = candidates['name']
-    if candidate in list1:
-        found = True
-        list2 = candidates['votes']
-        candidate_num = list1.index(candidate)
-        list2[candidate_num] = list2[candidate_num]+ 1 
-        print(f" {found}")
-        print(f"votes {votes}")
-        candidates.update({'votes': list2})
-        print(f" dic. {candidates}")
-    else:
-        found = False
-        votes = 1
-        print(f" {found}")
-        candidates['name'].append(candidate)
-        candidates['votes'].append(votes)
-        print(f" dic. {candidates}")
-    answer = input("Do you want to add another Candidate? y(es) or n(o)? ")
-print(f" candidates { candidates}")
+# create a dictionary placeholder
+
+candidate_dict = {}
+
+# open the data file
+with open (election_data_csv, newline = "") as csvfile:
+
+# read the data 
+    csvreader = csv.reader(csvfile, delimiter=",")
+
+    # read the header
+    csv_header = next(csvreader)
+    # inialize the total voites
+    total_votes = 0
+    # loop through the data file
+    for row in csvreader:
+        candidate = row[2]
+        total_votes +=1
+
+        # if the candidate is in the dictionary add to vote count
+        if candidate in candidate_dict:
+            vote = candidate_dict.get(candidate)
+            vote +=1
+            candidate_dict[candidate] = vote
+        # otherwise add the candidate and set the vote to 1
+        else:
+            candidate_dict[candidate] = 1
+print(f" total: {total_votes}")
+for candidates, votes in candidate_dict.items():
+    print (candidates, votes)
+
+
+# 
+# while answer =="y":
+#     candidate = input("Enter the Candidate Name:  ")
+
+#     print(f" from the keyboard: { candidate}")
+
+#     # if the candidate is the dictionary, then increment the voote
+#     if candidate in candidate_dict:
+#         vote = candidate_dict.get(candidate)
+#         vote +=1 
+#         candidate_dict[candidate] = vote
+#     else:
+#         candidate_dict[candidate] = 1
+#     answer = input("Do you want to add another Candidate? y(es) or n(o)? ")
+# print(f" candidate_dict { candidate_dict}")
